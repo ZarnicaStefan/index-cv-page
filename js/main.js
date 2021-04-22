be20006c441cf04017b8c92a1d470ce54d0bcb40
ZarnicaStefan <77352773+ZarnicaStefan@users.noreply.github.com>
2021-04-22 19:25:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //This toggles the nav
+    nav.classList.toggle("nav-active");
+
+    //This will give the links animations when clicked on the burger
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    //This will animate the handmade burger menu
+    burger.classList.toggle("burger-animation");
+  });
+};
+
+navSlide();
